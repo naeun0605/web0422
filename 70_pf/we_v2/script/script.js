432e22d5c796cc462a405486d74d9519073b1253
naeun0605 <ne9506@gmail.com>
2024-08-14 06:12:38 +0900

    0814

--- 70_pf/we_v2/script/script.js
+++ 70_pf/we_v2/script/script.js
@@ -1,21 +1,11 @@
-$(document).ready(function () {
-
-    // 모달팝업////////////////////////////////////
-
-    //요소 찾기 및 이벤트 처리
-    // 객체.이벤트(function(){})
-    // firstList.click()=>{};
-
-    // .notice a로 써도됨
+$(document).ready(function(){
     $('.notice li').eq(0).click(function(){
-        // css({속성:값, 속성:값,...})
-        $('#modal').css({display:"block"});
+        $('#modal').css({
+            display:"block"});
     });
-    
-    $('button').click(function(){
-        $('#modal').css({display:"none"});
-    });
-    
-    //////////////////////////////////////////////
 
+    $('button').click(function(){
+        $('modal').css({display:"none"});
+    });
+    
 });
