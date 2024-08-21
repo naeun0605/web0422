76b3080341b33fd98440815cdc81947a0bb8f180
naeun0605 <ne9506@gmail.com>
2024-08-22 05:14:37 +0900

    0822

---
+++ 70_pf/megabox_v2/js/mdata.js
@@ -0,0 +1,25 @@
+const mdata = {
+    // 영어 데이터
+    en: {
+        gnb: ["Moive", "Event", "CS Center", "Notice"],
+        mtit: ["MB", "MegaBox"],
+        stit: ["", "Movie", "Event", "CS Center"],
+        s2: {
+            img: ["assets/movie4.jpg","assets/movie3.jpg","assets/movie2.jpg","assets/movie1.jpg","assets/movie8.jpg","assets/movie7.jpg","assets/movie6.jpg","assets/movie5.jpg"],
+            tit: ["Pilot", "Deadpool and Wolverine", "Revolver", "Despicable Me 4", "Sarah's Hatching", "Inside Out 2", "Victory", "Detective Conan"]
+        }  
+    },
+    // 한국어 데이터
+    ko: {
+        gnb: ["영화", "이벤트", "고객센터", "공지사항"],
+        mtit: ["엠비", "메가박스"],
+        stit: ["","영화", "이벤트", "고객센터"],
+        s2: {
+            img: ["assets/movie1.jpg","assets/movie2.jpg","assets/movie3.jpg","assets/movie4.jpg","assets/movie5.jpg","assets/movie6.jpg","assets/movie7.jpg","assets/movie8.jpg"],
+            tit: ["파일럿", "데드풀과 울버린", "리볼버", "슈퍼배드4", "사라의 하츄핑", "인사이드 아웃2", "빅토리", "명탐정 코난"]
+        }  
+    },
+};
+
+// 기본 데이터 내보내기 - 1개만
+export default mdata;
