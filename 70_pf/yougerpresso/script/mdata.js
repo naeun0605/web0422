4da8e6e9279d454713ab9e6f5901d0bff5e103a2
Kimnaeun <ne9506@gmail.com>
2024-09-04 17:39:28 +0900

    0904

---
+++ 70_pf/yougerpresso/script/mdata.js
@@ -0,0 +1,25 @@
+const mdata = {
+    // 영어 데이터
+    en: {
+        gnb: ["Menu", "Event", "CS Center", "Notice"],
+        mtit: ["YP", "YOGERPRESSO"],
+        stit: ["", "Menu", "Event", "CS Center"],
+        s2: {
+            img: ["assets/COFFEE.jpg","assets/NON-COFFEE.jpg","assets/MERRY.png","assets/ICECREAM.png","assets/yoger.png","assets/BLENDED.jpg","assets/FRUIT&TEA.jpg","assets/DESSERT.jpg"],
+            tit: ["coffee", "non_coffee", "strawberry", "icecream", "yogurt", "blended", "fruit_tea", "dessert"]
+        }  
+    },
+    // 한국어 데이터
+    ko: {
+        gnb: ["메뉴", "이벤트", "고객센터", "공지사항"],
+        mtit: ["와이피", "요거프레소"],
+        stit: ["","메뉴", "이벤트", "고객센터"],
+        s2: {
+            img: ["assets/COFFEE.jpg","assets/NON-COFFEE.jpg","assets/MERRY.png","assets/ICECREAM.png","assets/yoger.png","assets/BLENDED.jpg","assets/FRUIT&TEA.jpg","assets/DESSERT.jpg"],
+            tit: ["커피", "넌커피", "딸기", "아이스크림", "요거트", "블렌디드", "과일차", "디저트"]
+        }  
+    },
+};
+
+// 기본 데이터 내보내기 - 1개만
+export default mdata;
